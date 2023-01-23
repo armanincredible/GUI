bcf0564ae095daec1efd4c8096a5ea67c64067e7
armanincredbile <mesterio2003@mail.ru>
2023-01-23 14:12:19 +0300

    add PluginAdapter

--- plugins/adapter.h
+++ plugins/adapter.h
@@ -4,11 +4,16 @@
 #include "../plugin.h"
 #include "../widget.h"
 
+#ifndef WIN32
+#include <dlfcn.h>
+#else
+#include <windows.h>
+#endif
+
 int WidgetAdapterPaint(WidgetManager* widget, QPainter* painter);
 WidgetManager* from_iwidget_to_widget(IWidget* widget);
 int controller_adapter_widget (Button* button, WidgetManager* widget);
 int ToolActivityAdapter(Tool* tool, QPainter* painter, Point p);
-
 
 class ToolAdapter: public Tool
 {
@@ -139,7 +144,102 @@ int ButtonAdapterPaint(Button* button, QPainter* painter)
     pmap.convertFromImage(button_adapter->src_);
 
     painter->drawPixmap(target, pmap, target);
-
 }
+
+class PluginAdapter
+{
+    IPlugin* iplugin_ = NULL;
+
+    IPlugin* (*get_plugin)() = NULL;
+    void* lib_ = NULL;
+    void (*destroy_plugin)() = NULL;
+
+    Tool* tool_ = NULL;
+
+    WidgetManager* props_ = NULL;
+
+    Button* tool_button_ = NULL;
+
+    Tool* make_tool_from_itool(ITool* itool)
+    {
+        ToolAdapter* tool_adapter = new ToolAdapter(itool);
+        return tool_adapter;
+    }
+
+    WidgetManager* make_widget_from_iwidget(IWidget* iwidget, WidgetManager* parent_widget, Layer* layer)
+    {
+        WidgetAdapter* widget_adapter = new WidgetAdapter(iwidget, parent_widget, layer);
+        return widget_adapter;
+    }
+
+    Button* make_button_from_ibutton(IPushButton* ibutton, Layer* layer)
+    {
+        ButtonAdapter* button_adapter = new ButtonAdapter(ibutton, layer);
+        return button_adapter;
+    }
+
+    int load_plugin(char* lib_name)
+    {
+        #ifndef WIN32
+           lib_ = dlopen(lib_name, RTLD_LAZY);
+        #else
+           lib_ = LoadLibrary(s);
+        #endif
+           if (!lib_) {
+             printf("cannot open library '%s'\n", lib_name);
+             return -1;
+           }
+        #ifndef WIN32
+           get_plugin = (IPlugin* (*)())dlsym(lib_, "get_plugin");
+           destroy_plugin = (void (*)()) dlsym(lib_, "destroy_plugin");
+        #else
+           get_plugin = (IPlugin* (*)())GetProcAddress((HINSTANCE)lib_, "get_plugin");
+           destroy_plugin = (void (*)())GetProcAddress((HINSTANCE)lib_, "destroy_plugin");
+        #endif
+           if (get_plugin == NULL || destroy_plugin == NULL) {
+             printf("cannot load functions\n");
+             return -1;
+           }
+
+           iplugin_ = get_plugin();
+    }
+
+    void delete_plugin()
+    {
+        if (destroy_plugin)
+        {
+            destroy_plugin();
+        }
+        if (lib_)
+        {
+            #ifndef WIN32
+               dlclose(lib_);
+            #else
+               FreeLibrary((HINSTANCE)lib_);
+            #endif
+        }
+    }
+
+
+    PluginAdapter(char* path, WidgetManager* parent_widget, Layer* layer)
+    {
+        load_plugin(path);
+        tool_ = make_tool_from_itool(iplugin_->get_tool());
+        props_ = make_widget_from_iwidget(iplugin_->get_props(), parent_widget, layer);
+        tool_button_ = make_button_from_ibutton(iplugin_->get_tool_button(), layer);
+    }
+};
+
+/*
+class PluginManager
+{
+    PluginAdapter** plugins_ = NULL;
+    int plugins_num = 0;
+
+    int add_plugin(PluginAdapter* plugin)
+    {
+
+    }
+};*/
 
 #endif // ADAPTER_H
